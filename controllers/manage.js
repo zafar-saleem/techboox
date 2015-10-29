16d9c4a7850cb91626e46fd7bcac3df1d7f6e4e7
Zafar Saleem <zafarsaleem3@gmail.com>
2015-10-29 21:33:54 +0100

    Added functionality to admin area

--- controllers/manage.js
+++ controllers/manage.js
@@ -5,6 +5,10 @@ var Category = require('../models/categoryModel');
 
 module.exports = function (router) {
 
+    router.get('/', function (req, res) {
+        res.render('manage/index');
+    });
+
     router.get('/books', function (req, res) {
     	Book.find({}, function (err, books) {
     		if (err) console.log(err);
@@ -17,10 +21,6 @@ module.exports = function (router) {
     	});
     });
 
-    router.get('/', function (req, res) {
-    	res.render('manage/index');
-    });
-
     router.get('/categories', function (req, res) {
     	Category.find({}, function (err, categories) {
     		if (err) console.log(err);
@@ -31,6 +31,105 @@ module.exports = function (router) {
 
     		res.render('manage/categories/index', model);
     	});
+    });
+
+    router.get('/books/add', function (req, res) {
+        Category.find({}, function (err, categories) {
+            if (err) console.log(err);
+
+            var model = {
+                categories: categories
+            };
+
+            res.render('manage/books/add', model);
+        });
+    });
+
+    router.post('/books', function (req, res) {
+        var title = req.body.title && req.body.title.trim();
+        var category = req.body.category && req.body.category.trim();
+        var author = req.body.author && req.body.author.trim();
+        var publisher = req.body.publisher && req.body.publisher.trim();
+        var price = req.body.price && req.body.price.trim();
+        var description= req.body.description && req.body.description.trim();
+        var cover = req.body.cover && req.body.cover.trim();
+
+        if (title == '' || price == '') {
+            req.flash('error', 'Please fillout required fields');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        if (isNaN(price)) {
+            req.flash('error', 'price must be a number');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        var newBook = new Book({
+            title: title,
+            category: category,
+            author: author,
+            publisher: publisher,
+            price: price,
+            description: description,
+            cover: cover
+        });
+
+        newBook.save(function (err) {
+            if (err) console.log(err);
+
+            req.flash('success', 'Book added');
+            res.location('/manage/books');
+            res.redirect('/manage/books');
+        });
+    });
+
+    router.get('/books/edit/:id', function (req, res) {
+        Category.find({}, function (err, categories) {
+            Book.findOne({ _id: req.params.id }, function (err, book) {
+                if (err) console.log(err);
+
+                var model = {
+                    book: book,
+                    categories: categories
+                };
+
+                res.render('manage/books/edit', model);
+            });
+        });
+    });
+
+    // Edit book
+    router.post('/books/edit/:id', function(req, res){
+        var title = req.body.title && req.body.title.trim();
+        var category = req.body.category && req.body.category.trim();
+        var author = req.body.author && req.body.author.trim();
+        var publisher = req.body.publisher && req.body.publisher.trim();
+        var price = req.body.price && req.body.price.trim();
+        var description= req.body.description && req.body.description.trim();
+        var cover = req.body.cover && req.body.cover.trim();
+
+
+        Book.update({_id: req.params.id}, {
+            title:title,
+            category: category,
+            author: author,
+            publisher: publisher,
+            price: price,
+            description: description,
+            cover: cover
+
+        }, function(err) {
+            if(err) {
+                console.log('update error', err);
+            }
+
+            req.flash('success', "Book Updated");
+            res.location('/manage/books');
+            res.redirect('/manage/books');
+        });
+
     });
 
 };
